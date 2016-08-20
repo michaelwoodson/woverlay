58418f2c784b9c70e01b8cbe40076426532f0998
Michael Woodson <mwoodson@fmyi.com>
2016-08-20 10:42:18 -0500

    Fix padding when converting ids to strings.

--- test/idhelper.test.js
+++ test/idhelper.test.js
@@ -6,7 +6,13 @@ let idhelper = require('../lib/idhelper');
 
 test('closest', function(t) {
 	let ids = [idmaker('01'), idmaker('43'), idmaker('62') , idmaker('7B'), idmaker('AA'), idmaker('CC'), idmaker('DD')];
-	t.ok(idhelper.closest(idmaker('FB'),ids).substring(0,2) == '01');
-	t.ok(idhelper.closest(idmaker('49'),ids).substring(0,2) == '43');
+	t.ok(idhelper.closest(idmaker('FB'),ids).substring(0,2) == '01', 'closest should wrap');
+	t.ok(idhelper.closest(idmaker('49'),ids).substring(0,2) == '43', 'closest should go backwards');
+	t.end();
+});
+
+
+test('converttoidstring', function(t) {
+	t.equals(idhelper.convertToIdString('1'), '00000000000000000000000000000000000000000000000000000000000000001', 'zero padding');
 	t.end();
 });
